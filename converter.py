3e7bd59f1c8a91b934fd541c18d802d6e3b4a1d2
lucywyman <wyman.lucy@gmail.com>
2014-06-18 22:10:59 -0700

    Things are mostly working, but not stupid proofed.  Beware (dun dun dun)

--- converter.py
+++ converter.py
@@ -2,9 +2,8 @@ from flask import Flask, render_template, request
 
 #Create the application
 app = Flask(__name__)
-#app.config.from_object(__name__)
 
-##Alphabet dictionary for bases greater than 10
+#Alphabet dictionary for bases greater than 10
 alphabet = {
 'a' : 11, 
 'b' : 12, 
@@ -42,37 +41,56 @@ def index():
 #Generate convert page with results
 @app.route('/convert', methods=['POST', 'GET'])
 def getinfo():
-    #Get information from html form (protip: this is what request.form does!)
+    #print "A"
+    newnumber = 0
+    """Get information from html form (protip: this is 
+what request.form does!)"""
     base1 = int(request.form['base1'])
     base2 = int(request.form['base2'])
-    #Number can't be forced to be int since it may have base greater than 10.  
-    #Python automatically types all input as string, so then all we have to do is make sure it's lower case so that the dictionary will work.
-    oldnumber = request.form['number'].lower()
-    #If the first base is greater than 10, convert number to decimal
-    #and change base1 to base 10.  Kind of a cheat, but it works.
+    
+    """Number can't be forced to be int since it may have 
+base greater than 10.  Python automatically types all 
+input as string, so then all we have to do is make sure 
+it's lower case so that the dictionary will work."""
+    number = request.form['number'].lower()
+    """If the first base is greater than 10, convert number to decimal
+and change base1 to base 10.  Kind of a cheat, but it works."""
     if (base1 > 10):
-        number = int(oldnumber, base1)
+        #print "A1"
+        newnumber = int(number, base1)
         base1 = 10
-    result = convert(base1, base2, number)
-    return render_template('/convert.html', number=oldnumber, base2=base2, result=result)
+    else:
+        #print "A2"
+        newnumber = number
+    #print base1, base2, newnumber
+    result = convert(base1, base2, newnumber)
+    #print result    
+    return render_template('/convert.html', number=number, base2=base2, result=result)
 
-#I got this algorithm from a great stackoverflow post here
-#(it's not just copy paste)
-#http://cs.stackexchange.com/questions/10318/the-math-behind-converting-from-any-base-to-any-base-without-going-through-base
+    """I got this algorithm from a great stackoverflow post here
+(it's not just copy paste)
+http://cs.stackexchange.com/questions/10318/the-math-behind-converting-from-any-base-to-any-base-without-going-through-base"""
 def toDigits(number, base2):
+    #print "D"
     result = []
     while number>0:
         result.insert(0, number%base2)
         number = number//base2
-    #for digit in result:
-     #   for item in alphabet:
-      #      if digit == alphabet.value:
-       #         digit = alphabet.key
-    #Since the number is now an array, we convert it to a string, concatenate the string, then change it back to an int.
-    result = int(''.join(map(str, result)))
+        #print number
+    count = 0
+    for digit in result:
+        count += 1
+        #print digit, count
+        if (digit > 10):
+            result[count-1] = str(unichr(int(digit)+86))
+    """Since the number is now an array, we convert it to a string, 
+concatenate the string, then change it back to an int"""
+    result = str(''.join(map(str, result)))
+    #print result
     return result
 
 def fromDigits(number, base1):
+    #print "C"
     s = str(number)
     digits = []
     n = 0
@@ -80,20 +98,28 @@ def fromDigits(number, base1):
         digits.append(int(digit))
     for d in digits:
         n = base1*n+d
+    #print n
     return n
-     
+
 #Now, using these two functions, we can actually convert stuff!      
 def convert(base1, base2, number):
-    #Some optimizations.  Thank you, Python.
-    if base1==10 and base2==2:
-        return(bin(number)[2:])
-    elif base1 == 10 and base2 == 8:
-        return(oct(number)[2:])
-    elif base1 == 10 and base2 == 16:
-        return(hex(number)[2:])
-    else:
-        result = toDigits(fromDigits(number, base1), base2)       
-        return result
+    #print "B"
+    #Some optimizations
+    result = toDigits(fromDigits(number, base1), base2)       
+    return result
+
+#Fancy error handlers, for fancy error pages
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error.html', message="404 Error: Page not found")
+
+@app.errorhandler(403)
+def forbidden_page(e):
+    return render_template('error.html', message="403 Error: Page forbidden")
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('error.html', message="500 Error: Internal Server Error")
 
 #Now, run the app!  Yay!
 if __name__ == '__main__':
