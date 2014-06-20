1a559d7fcbb9093a783b8b42f0341e2123166ad7
lucywyman <wyman.lucy@gmail.com>
2014-06-19 19:12:31 -0700

    Almost done\! Site is completely functional now, and just needs cute picture error pages

--- converter.py
+++ converter.py
@@ -40,10 +40,14 @@ alphabet = {
 def index():
     return render_template('index.html')
 
+#About page
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 #Generate convert page with results
 @app.route('/convert', methods=['POST', 'GET'])
 def getinfo():
-    #print "A"
     newnumber = 0
     """Get information from html form (protip: this is 
 what request.form does!)"""
@@ -52,7 +56,7 @@ what request.form does!)"""
         base2 = int(request.form['base2'])
     except ValueError:
         return render_template('index.html', error=1)
-    if (base1<1 or base2<1 or base1>36 or base2>36):
+    if ((base1<-36 or (base1>-1 and base1<1) or base1>36) or (base2<-36 or (base2>-1 and base2<1) or base2>36)):
         return render_template('index.html', error=1)   
     """Number can't be forced to be int since it may have 
 base greater than 10.  Python automatically types all 
@@ -62,41 +66,39 @@ it's lower case so that the dictionary will work."""
     """If the first base is greater than 10, convert number to decimal
 and change base1 to base 10.  Kind of a cheat, but it works."""
     if (base1 > 10):
-        #print "A1"
         newnumber = int(number, base1)
         base1 = 10
     else:
-        #print "A2"
         newnumber = number
-    #print base1, base2, newnumber
     result = convert(base1, base2, newnumber)
-    #print result    
     return render_template('/convert.html', number=number, base2=base2, result=result)
 
     """I got this algorithm from a great stackoverflow post here
 (it's not just copy paste)
 http://cs.stackexchange.com/questions/10318/the-math-behind-converting-from-any-base-to-any-base-without-going-through-base"""
 def toDigits(number, base2):
-    #print "D"
     result = []
-    while number>0:
-        result.insert(0, number%base2)
+    rem = 0
+    while number!=0:
+        rem = number%base2
         number = number//base2
-        #print number
+        #Uncomment at your own risk
+        #number, rem = divmod(number, base2)
+        if (rem < 0):
+            number = number+1
+            rem = rem+(-1*base2)
+        result.insert(0, rem)
     count = 0
     for digit in result:
         count += 1
-        #print digit, count
         if (digit > 10):
             result[count-1] = str(unichr(int(digit)+86))
     """Since the number is now an array, we convert it to a string, 
 concatenate the string, then change it back to an int"""
     result = str(''.join(map(str, result)))
-    #print result
     return result
 
 def fromDigits(number, base1):
-    #print "C"
     s = str(number)
     digits = []
     n = 0
@@ -104,13 +106,10 @@ def fromDigits(number, base1):
         digits.append(int(digit))
     for d in digits:
         n = base1*n+d
-    #print n
     return n
 
 #Now, using these two functions, we can actually convert stuff!      
 def convert(base1, base2, number):
-    #print "B"
-    #Some optimizations
     result = toDigits(fromDigits(number, base1), base2)       
     return result
 
